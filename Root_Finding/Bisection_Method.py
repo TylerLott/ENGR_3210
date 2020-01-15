ce927b14ff839db92ab0d430b30a25f16dec47f6
Tyler Lott <Tylerlott0126@gmail.com>
2020-01-15 09:01:50 -0700

    bisection method

---
+++ Root_Finding/Bisection_Method.py
@@ -0,0 +1,12 @@
+# applies bisection method and prints answer to screen
+
+XL = 1
+XU = 1
+ES = 1
+FUNCTION = '1'
+
+def bisection_method(xl, xu, es, imax, function):
+
+
+if __name__ == "__main__":
+    bisection_method(XL, XU, ES, FUNCTION)
