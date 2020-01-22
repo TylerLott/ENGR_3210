d26032a44a9923f03b094a3a22e77010a86dc2e5
Tyler Lott <Tylerlott0126@gmail.com>
2020-01-21 17:50:17 -0700

    done

--- Root_Finding/Bisection_Method.py
+++ Root_Finding/Bisection_Method.py
@@ -1,31 +1,36 @@
 # applies bisection method and prints answer to screen
 
-XL = 1
+XL = .5
 XU = 1
-ES = 1
-FUNCTION = '1'
+ES = .1
 
-def bisection_method(xl, xu, es, imax, function):
-    iter, xr, ea = 0, xl, es
+
+def bisection_method(xl, xu, es, imax):
+    iter = 0
+    xr = xl
+    ea = es
 
     while iter < imax:
         xr_old = xr
-        xr = (xl + xu)/ 2
+        xr = (xl + xu) / 2
         iter += 1
         if xr != 0:
             ea = abs((xr - xr_old) / xr) * 100
         else:
             print("error")
-        test = function(xl) + function(xr)
+        test = (xl**5 - (10 * xl**4) + (46 * xl**3) - (90 * xl**2) + (85 * xl) - 31) + \
+               (xr**5 - (10 * xr**4) + (46 * xr**3) - (90 * xr**2) + (85 * xr) - 31)
         if test < 0:
-            xu = xr
-        elif test > 0:
             xl = xr
+        elif test > 0:
+            xu = xr
         else:
             ea = 0
-        if ea < es or iter > imax:
+        if ea < es:
             break
     return xr
 
+
 if __name__ == "__main__":
-    bisection_method(XL, XU, ES, FUNCTION)
+    result = bisection_method(XL, XU, ES, 10000)
+    print(result)
